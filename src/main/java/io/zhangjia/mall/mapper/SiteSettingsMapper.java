34eed6ae286b1f6afcc8ff423299fa1893832008
zhangjia <zhangjia@188.com>
2019-07-19 00:11:39 +0800

    WalletMapper、SiteSettingsMapper、WalletMapper命名规范

--- src/main/java/io/zhangjia/mall/mapper/SiteSettingsMapper.java
+++ src/main/java/io/zhangjia/mall/mapper/SiteSettingsMapper.java
@@ -3,6 +3,18 @@ package io.zhangjia.mall.mapper;
 import org.apache.ibatis.annotations.Param;
 
 public interface SiteSettingsMapper {
+    /**
+     * 根据key查找value
+     * @param key
+     * @return
+     */
     String queryByKey(String key);
+
+    /**
+     * 根据key更新value
+     * @param key
+     * @param value
+     * @return
+     */
     Integer doUpdate(@Param("key") String key, @Param("value")String value);
 }
--- src/main/java/io/zhangjia/mall/mapper/WalletMapper.java
+++ src/main/java/io/zhangjia/mall/mapper/WalletMapper.java
@@ -5,7 +5,28 @@ import org.apache.ibatis.annotations.Param;
 import java.util.Map;
 
 public interface WalletMapper {
+    /**
+     * 添加钱包记录
+     * 场景：注册用户的时候，根据注册用户的id生成对应的钱包
+     * @param userId
+     * @return
+     */
     int doInsert(Integer userId);
+
+    /**
+     * 查询钱包信息
+     * @param userId
+     * @return
+     */
     Map<String,Object> queryByUserId(Integer userId);
-    int doUpdate(@Param("userId") Integer userId, @Param("money") Double money);
+
+    /**
+     * 更新钱包金额
+     * 场景1：支付的时候，扣钱，money为负数
+     * 场景2：充值的时候，加钱，moeny为正数
+     * @param userId
+     * @param money
+     * @return
+     */
+    int doUpdate(@Param("user_id") Integer userId, @Param("money") Double money);
 }
