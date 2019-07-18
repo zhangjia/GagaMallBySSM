6729f0163b9ab3a5d29eaef78bea830f62f09a37
zhangjia <zhangjia@188.com>
2019-07-18 23:48:19 +0800

    MenuMapper规范

--- src/main/java/io/zhangjia/mall/mapper/MenuMapper.java
+++ src/main/java/io/zhangjia/mall/mapper/MenuMapper.java
@@ -5,6 +5,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface MenuMapper {
+    /**
+     * 查询一级菜单
+     * @return
+     */
     List<Map<String,Object>> queryLevel1Menu();
+
+    /**
+     * 查询二级菜单
+     * 将查询结果作为一级菜单的一个属性，所以下面是灰色
+     * @return
+     */
     List<Map<String,Object>> queryLevel2Menu(Integer id);
 }
